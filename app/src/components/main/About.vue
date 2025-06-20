2c10316439a8d1b031a98eb6f8edf1a8f1cc7f29
KoZeuh <kozeuhdev@gmail.com>
2025-06-20 14:06:18 +0200

    💄 Update title in About section to reflect correct application name

--- app/src/components/main/About.vue
+++ app/src/components/main/About.vue
@@ -2,7 +2,7 @@
     <section id="about" class="bg-gray-100 py-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="text-center">
-                <h2 class="text-3xl md:text-4xl font-bold text-gray-900 mb-8">À Propos de MyBurger</h2>
+                <h2 class="text-3xl md:text-4xl font-bold text-gray-900 mb-8">À Propos de VueMyBurger</h2>
                 <div class="max-w-3xl mx-auto">
                     <p class="text-lg text-gray-600 mb-6">
                     Designé pour être mangé. Codé pour être aimé.<br />
